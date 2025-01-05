8b8f238e9cad1ef1e8618a3f522bacda79bddb71
Shaik Mohammed <nisashap@amazon.com>
2025-01-05 14:23:11 +0530

    second condition of data

--- 03-varibles2.sh
+++ 03-varibles2.sh
@@ -2,7 +2,8 @@
 
 PERSON1=RAMESH # no space before and afer equal
 PERSON2=SURESH 
-ech "$PERSON1 :: hi $PERSON2"
-ech "$PERSON2 :: hello $PERSON1"
+
+ech "$PERSON1:: hi $PERSON2"
+ech "$PERSON2:: hello $PERSON1"
 echo "$PERSON1:: How are you doing?"
-echo "$PERSON2 :: i am good . How are you?"
+echo "$PERSON2:: i am good. How are you?"
---
+++ 09-condition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt, -lt, -eq -le,
+if [ $NUMBER -gt ]
+then
+echo "Given number is greater than 100"
+else
+echo "Given number is less than or aqual to 100"
+fi
