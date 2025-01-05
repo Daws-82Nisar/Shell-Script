eb6361b4750a587291a95423585819a247e253cc
Shaik Mohammed <nisashap@amazon.com>
2025-01-05 13:22:45 +0530

    this my data type

---
+++ 08-array-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+movies=("Pushpa" "RRR" "Devera")
+# index start from 0, size is 3
+
+echo "First Movie: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
+
+echo "All Movies are: ${MOVIES[@]}"
